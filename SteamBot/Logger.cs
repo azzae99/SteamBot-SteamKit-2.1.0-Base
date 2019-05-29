8cc487738ca1644bb1aec5ea50eedada75ecbe44
unpaid <35709143+unpaid@users.noreply.github.com>
2019-05-30 04:13:58 +1000

    Minor changes to the Logger class to prevent certain errors from happening

--- SteamBot/Logger.cs
+++ SteamBot/Logger.cs
@@ -11,49 +11,48 @@ namespace SteamBot
         public Logger(string username)
         {
             Username = username;
-            Directory.CreateDirectory("00_Logs");
-            Stream = File.AppendText(Path.Combine("00_Logs", Username + ".log"));
+            Directory.CreateDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "00_Logs"));
+            Stream = File.AppendText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "00_Logs", $"{Username}.log"));
             Stream.AutoFlush = true;
         }
 
         public void Debug(string output, params object[] args)
         {
-            OutputLine(ConsoleColor.DarkGray, String.Format("[DEBUG]: {0}", String.Format(output, args)));
+            OutputLine(ConsoleColor.DarkGray, $"[DEBUG]: { (args.Length == 0 ? output : String.Format(output, args)) }");
         }
 
         public void Info(string output, params object[] args)
         {
-            OutputLine(ConsoleColor.White, String.Format("[INFO]: {0}", String.Format(output, args)));
+            OutputLine(ConsoleColor.White, $"[INFO]: { (args.Length == 0 ? output : String.Format(output, args)) }");
         }
 
         public void Success(string output, params object[] args)
         {
-            OutputLine(ConsoleColor.Green, String.Format("[SUCCESS]: {0}", String.Format(output, args)));
+            OutputLine(ConsoleColor.Green, $"[SUCCESS]: { (args.Length == 0 ? output : String.Format(output, args)) }");
         }
 
         public void Warn(string output, params object[] args)
         {
-            OutputLine(ConsoleColor.Yellow, String.Format("[WARNING]: {0}", String.Format(output, args)));
+            OutputLine(ConsoleColor.Yellow, $"[WARNING]: { (args.Length == 0 ? output : String.Format(output, args)) }");
         }
 
         public void Error(string output, params object[] args)
         {
-            OutputLine(ConsoleColor.Red, String.Format("[ERROR]: {0}", String.Format(output, args)));
+            OutputLine(ConsoleColor.Red, $"[ERROR]: { (args.Length == 0 ? output : String.Format(output, args)) }");
         }
 
         public void Prompt(string output, params object[] args)
         {
-            OutputLine(ConsoleColor.Magenta, String.Format("[PROMPT]: {0}", String.Format(output, args)));
+            OutputLine(ConsoleColor.Magenta, $"[PROMPT]: { (args.Length == 0 ? output : String.Format(output, args)) }");
         }
 
         public void OutputLine(ConsoleColor color, string output)
         {
-            string conOutput = String.Format("{0} {1} {2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Username, output);
-            string fileOutput = String.Format("{0} {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), output);
+            string Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
             Console.ForegroundColor = color;
-            Console.WriteLine(conOutput);
+            Console.WriteLine($"{Timestamp} {Username} {output}");
             Console.ForegroundColor = ConsoleColor.White;
-            Stream.WriteLine(fileOutput);
+            Stream.WriteLine($"{Timestamp} {output}");
         }
     }
 }
